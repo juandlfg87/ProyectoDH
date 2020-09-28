c7a6722d12d2e94b2ce7ada9d965389cd998bcaf
Juan de la Fuente <juandelafuente@hendercross.com>
2020-09-27 21:04:51 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const R = require ('ramda');
+
+const mult = require ('./multiplicar');
+
+console.log(mult);
+
+/*
+const primero = R.add(1);
+
+console.log(R);
+
+console.log(primero(2));
+
+console.log(R.add(1)(7));
+*/
